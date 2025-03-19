2654da5e5353dc8a4a27527a544117dd680ef071
Aaditya Nair <aaditya.nair2412@gmail.com>
2025-03-19 18:43:26 +0000

    added post routes and controllers

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -51,7 +51,14 @@ const userSchema = new mongoose.Schema({
     link: {
         type:String,
         default: '',
-    }
+    },
+    likedPosts:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post",
+            default: []
+        },
+    ],
 }, { timestamps: true });
 
 const User = mongoose.model("User", userSchema); // even though we have declared it as User in mongoose it will be labelled as users
