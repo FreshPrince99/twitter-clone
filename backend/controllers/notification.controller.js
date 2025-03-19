34ec144e53e985908cf708db5113a15236d6857d
Aaditya Nair <aaditya.nair2412@gmail.com>
2025-03-19 19:43:10 +0000

    notification routes and controllers added

---
+++ backend/controllers/notification.controller.js
@@ -0,0 +1,37 @@
+import Notification from "../models/notification.model.js";
+import User from "../models/user.model.js";
+
+export const getNotifications = async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        const notifications = await Notification.find({to: userId})
+        // .sort({ createdAt: -1 })
+        .populate({
+            path: "from",
+            select: "username profileImg"
+        });
+
+        await Notification.updateMany({ to: userId}, {read: true});
+
+        res.status(200).json(notifications)
+
+    } catch (error) {
+        console.log("Error in getNotifications function: ", error.message);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+export const deleteNotifications = async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        await Notification.deleteMany({ to: userId });
+
+        res.status(200).json({ message: "Notifications deleted successfully" });
+
+    } catch (error) {
+        console.log("Error in deleteNotifications function: ", error.message);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+};
